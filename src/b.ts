ea112baf8b21fa780cb45622a6edc62934dfcbc0
Jorge14288 <jorge.fuentes.14288@gmail.com>
2018-03-19 10:45:53 -0600

    Se agrego el archivo de configuraciones b para los endpoints

---
+++ src/b.ts
@@ -0,0 +1,15 @@
+export class b {
+    static get API_ENDPOINT() : string{
+        return 'https://middlesitetse.azurewebsites.net/api';
+    }
+    static get TIMEOUT(): number {
+        return 10000
+      }
+      static get C_OK(): number {
+        return 0
+      }
+    
+      static get C_ERR(): number {
+        return 1
+      }
+}
--- src/providers/tse/tse.ts
+++ src/providers/tse/tse.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { b } from '../../b';
 
 /*
   Generated class for the TseProvider provider.
@@ -14,12 +16,21 @@ export class TseProvider {
   constructor(public http: HttpClient) {
     console.log('Hello TseProvider Provider');
   }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'my-auth-token',
+      timeout: '${20000}'
+    })
+  };
 
   getCapcha(solicitud) {
+    console.log("Endpoint traido de el archivo v"+ b.API_ENDPOINT );
     return new Promise((resolve, reject) => {
      //this.http.post('http://165.227.57.200:3000/api/tse/captcha', solicitud)https://middlesitetse.azurewebsites.net
      //this.http.post('http://localhost:51243/api/getCapcha', solicitud)
-     this.http.post('https://middlesitetse.azurewebsites.net/api/getCapcha', solicitud)
+     this.http.post( b.API_ENDPOINT+'/getCapcha', solicitud, this.httpOptions)
+    
         .subscribe(res => {
           console.log("Resolve", res);
           resolve(res);
@@ -33,7 +44,7 @@ export class TseProvider {
     return new Promise((resolve, reject) => {
 
     // this.http.post('http://165.227.57.200:3000/api/tse/lugarVotacion', data)
-    this.http.post('https://middlesitetse.azurewebsites.net/api/obtenerLugar', data)
+    this.http.post(b.API_ENDPOINT+'/obtenerLugar', data, this.httpOptions)
         .subscribe(res => {
           console.log("Resolve", res);
           resolve(res);
