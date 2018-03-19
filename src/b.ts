e676eee5565799bfff4d03843c190881ebad9053
Jorge14288 <jorge.fuentes.14288@gmail.com>
2018-03-19 17:58:45 -0600

    en el campo captcha se modifico para que acepte solo minusculas

--- src/b.ts
+++ src/b.ts
@@ -1,6 +1,7 @@
 export class b {
     static get API_ENDPOINT() : string{
-        return 'https://middlesitetse.azurewebsites.net/api';
+      //  return 'https://middlesitetse.azurewebsites.net/api';
+        return 'http://middlesitetse.azurewebsites.net/api';
     }
     static get TIMEOUT(): number {
         return 10000
